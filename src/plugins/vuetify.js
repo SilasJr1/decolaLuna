84f83797332ace64a8125d2ad9669d9f4d96a784
Jainy <jainyestefany@gmail.com>
2022-03-19 12:21:45 -0300

    Menu e Dark Mode

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.green.accent3,
+                secondary: colors.deepOrange.lighten1,
+            },
+            dark: {
+                primary: "#90CAF9",
+                secondary: "#E91E63",
+            },
+        },
+    },
 });
