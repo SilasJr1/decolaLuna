a441a86320c35ec90d6808160602130cbfdb301e
Silas Gonçalves <silas.henrique@gmail.com>
2022-03-19 13:40:34 -0300

    ListaSites

--- src/views/ListaVerificacao.vue
+++ src/views/ListaVerificacao.vue
@@ -1,13 +1,36 @@
 <template>
-  <h1>Teste</h1>
+    <v-container class="sites-lista">
+        <h2 class="text-h5 text-center mb-3 mt-5">Lista de sites para verificação de notícias</h2>
+		<div v-for="site of sitesLista" :key="site.id">
+			<SitesCard :site="site" />
+		</div>
+    </v-container>
 </template>
 
 <script>
+import SitesCard from "../components/SitesCard.vue";
 export default {
-
-}
+    name: "ListaVerificacao",
+    components: {
+        SitesCard,
+    },
+    data: () => ({
+        sitesLista: [],
+    }),
+    created() {
+        fetch("https://it3-hbn-default-rtdb.firebaseio.com/fakeNews.json")
+            .then((response) => response.json())
+            .then((json) => {
+                this.sitesLista = json;
+            });
+    },
+};
 </script>
 
 <style scoped>
+
+h2 {
+	margin-bottom: 20px;
+}
 
 </style>
