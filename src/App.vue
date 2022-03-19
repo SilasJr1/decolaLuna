7e8fd75408cd6162e7216a8c910093be4c093ce6
Silas Gonçalves <silas.henrique@gmail.com>
2022-03-19 10:51:25 -0300

    Definicao de paleta de cores

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
 }
 
 nav {
@@ -21,10 +21,11 @@ nav {
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #01579B;
+    text-decoration: none;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #B71C1C;
     }
   }
 }
